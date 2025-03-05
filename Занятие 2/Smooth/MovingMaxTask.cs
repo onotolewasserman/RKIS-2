350ad1bc9674c1ae8543301c39e2fa773e4c3613
onotolewasserman <alwm2006@hotmail.com>
2025-03-05 23:32:47 +0300

    Update MovingMaxTask.cs

--- Занятие 2/Smooth/MovingMaxTask.cs
+++ Занятие 2/Smooth/MovingMaxTask.cs
@@ -2,13 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace yield;
-
-public static class MovingMaxTask
+namespace yield
 {
-	public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
-	{
-		//Fix me!
-		return data;
-	}
+
+    public static class MovingMaxTask
+    {
+        public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
+        {
+            var window = new Queue<double>();
+            var list = new LinkedList<double>();
+
+            foreach (var point in data)
+            {
+                window.Enqueue(point.OriginalY);
+
+                if (window.Count > windowWidth)
+                {
+                    if (window.Dequeue() >= list.First.Value)
+                    {
+                        list.RemoveFirst();
+                    }
+                }
+
+                while (list.Count != 0 && point.OriginalY > list.Last.Value)
+                {
+                    list.RemoveLast();
+                }
+
+                list.AddLast(point.OriginalY);
+
+                yield return point.WithMaxY(list.First.Value);
+            }
+
+
+        }
+    }
 }
